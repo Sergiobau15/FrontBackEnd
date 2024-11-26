cfb6e7ed768d5bff9b2fb5afed4ecbf6b0b3ec8f
jerson-m <159957614+jerson-m@users.noreply.github.com>
2024-11-25 19:57:54 -0500

    Add files via upload

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, createOrdercar, orders, getOrderById, deleteOrder, editOrder  } = require('../controllers/order');
+const { createOrder, createOrdercar, orders, getOrdersByUserId, deleteOrder, editOrder  } = require('../controllers/order');
 
 router.post("/createcar", createOrdercar);
 router.post("/create", createOrder);
 router.get("/", orders);
-router.get("/:id", getOrderById );
+router.get("/user/:userId", getOrdersByUserId);
 router.put("/edit/:id", editOrder);
 router.get('/desactivate/:id', deleteOrder );
 
